11496d0903320f88294919fca99cc6b22d831043
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-10-20 18:26:02 +0200

    Task 29.4 Zadanie: Rozbuduj swój endpoint

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,6 @@ public class EmailScheduler {
     private AdminConfig adminConfig;
 
     @Scheduled(fixedDelay = 10000)
-    //(cron = "0 0 10 * * *")
     public void SendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
