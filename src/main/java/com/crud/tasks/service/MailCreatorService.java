5736cd7d06cca1beac1a08564fa32be5bafa7d26
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-11-05 09:23:00 +0100

    Task 32.3 Wykonaj szablon maila

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -19,13 +22,21 @@ public class MailCreatorService {
 
 
     public String buildTrelloCardEmail(String message){
+        List<String> motivation = new ArrayList<>();
+        motivation.add("Yes, you can do it!");
+        motivation.add("Everything is possible!");
+        motivation.add("The world is not enough!");
+
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
-        context.setVariable("button", "Visit website");
+        context.setVariable("tasks_url", "https://trello.com/");
+        context.setVariable("button", "Click here and go to work!");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("good_bye", "See you soon!");
-        context.setVariable("admin_company", adminConfig.getAdminCompany());
+        context.setVariable("good_bye", "May the force be with you.... :)");
+        context.setVariable("show_button", true);
+        context.setVariable("is_male", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_motivation", motivation);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
